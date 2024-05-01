a5ca277110b94eb6016a26330ab014cc8fd45713
vijay <vijaysakhare441991@gmail.com>
2024-05-01 18:34:38 +0530

    Datatypesinjava

---
+++ src/main/java/org/example/datatypesinjava/ByteDAtaType.java
@@ -0,0 +1,27 @@
+package org.example.datatypesinjava;
+
+public class ByteDAtaType {
+    public static void main(String[] args) {
+
+
+        Byte mByteValue = 100;
+        byte myByte = 100;
+        short myShort = 10000;
+        int myInt = 1000000;
+        long myLong = 1000000000L; // Note the 'L' suffix for long literals
+        float myFloat = 3.14f; // Note the 'f' suffix for float literals
+        double myDouble = 3.14159;
+        boolean myBoolean = true;
+        char myChar = 'A';
+
+        System.out.println("byte: " + myByte);
+        System.out.println("short: " + myShort);
+        System.out.println("int: " + myInt);
+        System.out.println("long: " + myLong);
+        System.out.println("float: " + myFloat);
+        System.out.println("double: " + myDouble);
+        System.out.println("boolean: " + myBoolean);
+        System.out.println("char: " + myChar);
+    }
+
+}
